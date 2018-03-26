94dea6074a4fc2d0ea6918aaed65f348f9fbc8ec
Blaze Sanders <blaze@infinifill.com>
2018-03-26 04:18:49 -0700

    Added example complex HTML from ROBO BEV www.webflow.io still need to simplify

--- main/userLogin.c
+++ main/userLogin.c
@@ -19,6 +19,27 @@
 #include "userLogin.h"
 #include <string>
 
+
+// static index page
+static const char http_index[] =
+    "HTTP/1.1 200 OK\r\n"
+    "Content-type: text/html\r\n\r\n"
+    "<html>\r\n"
+    "<head>\r\n"
+    "<title>AirBox User Login Screen</title>\r\n"
+    "</head>\r\n"
+    "<body>\r\n"
+    //Used www.webflow.io to generate HTML
+    "<div depth=\"5\" tabindex=\"0\" style=\"position: absolute; top: 0px; left: 0px; width: 240px; opacity: 1; background: rgb(64, 64, 64); transform: translate3d(0px, 225px, 0px); outline: 0px; cursor: default; user-select: none; display: flex; align-items: center; box-sizing: border-box; padding-right: 10px; padding-left: 82px; line-height: 24px;\"><div style=\"box-sizing: border-box; flex-shrink: 0; width: 18px; height: 24px; padding-right: 6px; display: flex; align-items: center; justify-content: center;\"></div><div style=\"flex: 1 1 0%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;\"><div style=\"display: flex; align-items: center;\"><div type=\"regular\" style=\"color: rgb(217, 217, 217); padding-right: 6px;\"><svg width="12" height="12" viewBox="0 0 12 12" class="bem-Svg " style="display: block; transform: translate(0px, 0px);"><path fill="currentColor" d="M10 2v8H2V2h8m1-1H1v10h10V1z"></path><path fill="currentColor" d="M4 3H3v6h1V3" opacity=".35"></path></svg></div><div style="flex: 1 1 0%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"><div style="display: flex; align-items: center; flex: 0 1 auto;"><div style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">Form Field</div></div></div></div></div></div>
+
+    "</body>\r\n"
+    "</html>\r\n\r\n";
+
+
+
+
+
+
 //See userLogin.h for futher documentation on the following PUBLIC functions:
 
 void InitializeDatabase(??? *db){
